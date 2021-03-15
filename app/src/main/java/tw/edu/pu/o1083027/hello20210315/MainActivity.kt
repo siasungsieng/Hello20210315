7c194fc1e9b46177b5553b9b7cff0a9da17dd00e
谢纯善 <o1083027@gm.pu.edu.tw>
2021-03-15 10:52:05 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/tw/edu/pu/o1083027/hello20210315/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083027/hello20210315/MainActivity.kt
@@ -2,10 +2,16 @@ package tw.edu.pu.o1083027.hello20210315
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello money"
+
     }
 }
