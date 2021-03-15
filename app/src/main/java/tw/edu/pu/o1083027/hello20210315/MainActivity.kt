58cdb00c4623751653eff7665a96f078cc217e7d
谢纯善 <o1083027@gm.pu.edu.tw>
2021-03-15 11:59:20 +0800

    修改文字

--- app/src/main/java/tw/edu/pu/o1083027/hello20210315/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083027/hello20210315/MainActivity.kt
@@ -8,10 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello money"
-
+        txv.text = "Hello money money"
     }
 }
