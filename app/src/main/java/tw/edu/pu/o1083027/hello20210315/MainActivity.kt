4cbd7d581bb8509dccdadba70fb087431903be78
谢纯善 <o1083027@gm.pu.edu.tw>
2021-03-15 10:43:55 +0800

    第一次commit

---
+++ app/src/main/java/tw/edu/pu/o1083027/hello20210315/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.o1083027.hello20210315
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
